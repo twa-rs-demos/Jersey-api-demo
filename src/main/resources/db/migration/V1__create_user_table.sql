9862eec2c925afdb71d93b68a76e7553f1b6a970
zhangpei <13488233237@163.com>
2016-10-26 10:15:36 +0800

    add flyway migration file

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,11 +1,3 @@
---#create database
---DROP DATABASE IF EXISTS UserDB;
---CREATE DATABASE UserDB;
---
---#useUserDB
---USE UserDB;
-
-# create user_info_table
 DROP TABLE IF EXISTS users;
 CREATE TABLE  users (
     id int not null primary key AUTO_INCREMENT,  #自增
