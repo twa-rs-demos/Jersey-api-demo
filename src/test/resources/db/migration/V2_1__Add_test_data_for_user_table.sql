3890ad9eb109d90617cfe136651d171123c836b8
zhangpei <13488233237@163.com>
2016-10-26 10:16:49 +0800

    modify the migration about insert data

--- src/test/resources/db/migration/V2_1__Add_test_data_for_user_table.sql
+++ src/test/resources/db/migration/V2_1__Add_test_data_for_user_table.sql
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+INSERT INTO users(mobilePhone, email, password) VALUES( "18829290576", "565678150@qq.com", "123456789");
+INSERT INTO users(mobilePhone, email, password) VALUES( "17791378995", "357283632@qq.com", "123456789");
