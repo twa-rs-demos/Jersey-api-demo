776d6fbba1fb86bb0b27dd1231dfa95c1d345d2c
Afaren <550047450@qq.com>
2016-10-28 12:11:37 +0800

    afaren - add flyway clean & change gradle wrapper version

--- src/test/java/com/thoughtworks/twars/mapper/TestBase.java
+++ src/test/java/com/thoughtworks/twars/mapper/TestBase.java
@@ -20,9 +20,9 @@ public class TestBase {
     @Before
     public void setUp() throws Exception {
         session.startManagedSession();
-        //// FIXME: 16-10-27 unknown database 'UserDB'
-//        flyway.clean();
-//        flyway.migrate()/**/;
+        // FIXME: 16-10-27 unknown database 'UserDB'
+        flyway.clean();
+        flyway.migrate();
     }
 
     @After
