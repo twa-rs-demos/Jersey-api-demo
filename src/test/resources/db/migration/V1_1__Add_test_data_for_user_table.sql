28f8ad85bd649bd6c6c05bd62767a1bfda969e62
Afaren <550047450@qq.com>
2016-10-31 14:51:06 +0800

    afaren - fix flyway sql version file

--- src/test/resources/db/migration/V1_1__Add_test_data_for_user_table.sql
+++ src/test/resources/db/migration/V1_1__Add_test_data_for_user_table.sql
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-INSERT INTO users(mobilePhone, email, password) VALUES( "18829290576", "565678150@qq.com", "123456789");
-INSERT INTO users(mobilePhone, email, password) VALUES( "13391378995", "357283632@qq.com", "123456789");
-INSERT INTO users(mobilePhone, email, password) VALUES( "14491378995", "357283632@qq.com", "123456789");
-INSERT INTO users(mobilePhone, email, password) VALUES( "19991378995", "357283632@qq.com", "123456789");
+TRUNCATE users;
+INSERT INTO users values(null,  "18798037893", "test@163.com","550e1bafe077ff0b0b67f4e32f29d751", 1451117203);
+INSERT INTO users values(null,  "18087839393","test2@qq.com", "550e1bafe077ff0b0b67f4e32f29d751", 1451117300);
+INSERT INTO users values(null,  "18740404040","wjj@qq.com", "550e1bafe077ff0b0b67f4e32f29d751", 1451368017);
+INSERT INTO users values(null,  "18730304826","ydp@qq.com",  "550e1bafe077ff0b0b67f4e32f29d751", 1451370576);
+INSERT INTO users values(null,  "18740362964","lwj@qq.com", "550e1bafe077ff0b0b67f4e32f29d751", 1451370686);
+INSERT INTO users values(null,  "18291895012","z@z.com", "550e1bafe077ff0b0b67f4e32f29d751", 1452491001);
--- src/test/resources/db/migration/V2_1__Add_test_data_for_user_table.sql
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-TRUNCATE users;
-INSERT INTO users values(null, "test@163.com", "18798037893", "550e1bafe077ff0b0b67f4e32f29d751", 1451117203);
-INSERT INTO users values(null, "test2@qq.com", "18087839393", "550e1bafe077ff0b0b67f4e32f29d751", 1451117300);
-INSERT INTO users values(null, "wjj@qq.com", "18740404040", "550e1bafe077ff0b0b67f4e32f29d751", 1451368017);
-INSERT INTO users values(null, "ydp@qq.com", "18730304826", "550e1bafe077ff0b0b67f4e32f29d751", 1451370576);
-INSERT INTO users values(null, "lwj@qq.com", "18740362964", "550e1bafe077ff0b0b67f4e32f29d751", 1451370686);
-INSERT INTO users values(null, "z@z.com", "18291895012", "550e1bafe077ff0b0b67f4e32f29d751", 1452491001);
