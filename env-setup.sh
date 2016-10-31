52572941be886d4020257958f7e951c5a75adeb9
Afaren <550047450@qq.com>
2016-10-31 12:12:59 +0800

    afaren - modify DB config

--- env-setup.sh
+++ env-setup.sh
@@ -6,9 +6,6 @@ BASE_DIR=$(dirname $0)
 USER_API=user-api
 CONTAINER=assembly_mysql_1
 
-#function createContainer(){
-#    docker run -p 3306:3306 --name USER_API -e MYSQL_ROOT_PASSWORD=thoughtworks -d mysql:5.7
-#}
 
 function initMysql(){
   echo "the password of root:"
@@ -17,11 +14,5 @@ function initMysql(){
   docker exec -it $CONTAINER mysql -u root -pthoughtworks -e "$sql"
 }
 
-
-# 创建容器
-
-#createContainer
-
-# 初始化数据库，密码是 thoughtworks
 
 initMysql
